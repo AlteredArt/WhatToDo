8227c487070bde503cef3e38fc124547c8392972
Altered Art <cherokeedreemer@gmail.com>
2020-08-22 15:43:46 -0600

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import {FormsModule} from '@angular/forms';
+import { AboutComponent } from './components/pages/about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
     HeaderComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
